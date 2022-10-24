82eabcefc3d7c7e0c667115aac6938ba57a81e36
stalwartBunny <constructingintelligence@gmail.com>
2022-10-24 15:40:24 -0500

    uploaded tutorial code

---
+++ countingUp.py
@@ -0,0 +1,15 @@
+#def countUp():
+#    inputValue = int(input("Add your input here>> "))
+#    i = 0
+#    while i < 10:
+#        i = i + 1
+#        if i == 10:
+#            print(f"{inputValue + i}")
+#        else:
+#            print(f"{inputValue + i} then ")
+
+def countUp():
+    inputValue = int(input("Add your input here>> "))
+    print(f"{inputValue + 1} then {inputValue + 2} then {inputValue + 3} then {inputValue + 4} then {inputValue + 5} then {inputValue + 6} then {inputValue + 7} then {inputValue + 8} then {inputValue + 9} then {inputValue + 10}.")
+
+countUp()
